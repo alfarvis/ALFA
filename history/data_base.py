e6ff8c43e73ab29478afd63cb60003c1c57af6d9
garimella.gowtham74@gmail.com <garimella.gowtham74@gmail.com>
2018-01-07 13:23:59 -0800

    Merge data, keyword list into one object

--- history/data_base.py
+++ history/data_base.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python
 from keyword_search import KeywordSearch
 
+class DataObject(object):
+    def __init__(self, data, keyword_list):
+        self.data = data
+        self.keyword_list = keyword_list
+
 class Database(object):
     def __init__(self):
         self.keyword_search = KeywordSearch()
-        self.data_base = []
-        self.data_keyword_lists = []
+        self.data_objects = []
 
     def add(self, keyword_list, data_object):
-        self.keyword_search.add(keyword_list, len(self.data_base))
-        self.data_base.append(data_object)
-        self.data_keyword_lists.append(keyword_list)
+        self.keyword_search.add(keyword_list, len(self.data_objects))
+        self.data_objects.append(DataObject(data_object, keyword_list))
 
     def search(self, keyword_list):
         index_list = self.keyword_search.search(keyword_list)
-        return [self.data_base[index] for index in index_list]
+        return [self.data_objects[index].data for index in index_list]
 
     def discard(self, index):
-        self.keyword_search.discard(self.data_keyword_lists[index], index)
-        del self.data_base[index]
-        del self.data_keyword_lists[index]
+        self.keyword_search.discard(self.data_objects[index].keyword_list, index)
+        del self.data_objects[index]
