80210fe8b9fb54a786a39a6d22732501577a0079
Vishwa Parekh <vishwaparekh@gmail.com>
2018-06-02 03:31:07 -0400

    updating the branch

--- Alfarvis/data_handlers/read_csv.py
+++ Alfarvis/data_handlers/read_csv.py
@@ -58,7 +58,7 @@ class ReadCSV(AbstractReader):
             # categorical variable
             percCutoff_for_categorical = 0.1 # current cutoff is 10%
             
-            if (len(uniqVals)/len(col_data)) <= percCutoff_for_categorical:
+            if (len(uniqVals)/len(col_data)) <= percCutoff_for_categorical and isinstance(col_data[0],str)==True:
                 result_objects = self.add_categories_as_columns(uniqVals,col_data,col_split,keyword_list,result_objects,command_status)
             
         return result_objects
@@ -70,12 +70,12 @@ class ReadCSV(AbstractReader):
             arrays
         """
         for uniV in uniqVals:                
-                categ_data = col_data == uniV
-                categ_name = "group class " + str(uniV)
-                category_split = [key_val.lower()
-                             for key_val in self.pattern.split(categ_name)]
-                category_keyword_list = category_split+col_split+keyword_list
-                result_object = ResultObject(
-                categ_data*1, category_keyword_list, DataType.logical_array, command_status)
-                result_objects.append(result_object)
-                return result_objects
+            categ_data = col_data == uniV
+            categ_name = "group class " + str(uniV)
+            category_split = [key_val.lower()
+                         for key_val in self.pattern.split(categ_name)]
+            category_keyword_list = category_split+col_split+keyword_list
+            result_object = ResultObject(
+            categ_data*1, category_keyword_list, DataType.logical_array, command_status)
+            result_objects.append(result_object)
+        return result_objects
