0d50c334492fe9efec2950a99b3bba26cab7d016
garimella.gowtham74@gmail.com <garimella.gowtham74@gmail.com>
2018-06-04 09:40:23 -0400

    Add list column command

--- Alfarvis/commands/Stat_Container.py
+++ Alfarvis/commands/Stat_Container.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import numpy as np
 
 
 class StatContainer(object):
@@ -8,3 +9,15 @@ class StatContainer(object):
     output of commands etc
     """
     ground_truth = None
+    percCutoff_for_categorical = 0.1
+
+    @classmethod
+    def isCategorical(self, array):
+        """
+        Check if a array is categorical
+        """
+        uniqVals = np.unique(array)
+        if ((len(uniqVals) / len(array)) < self.percCutoff_for_categorical and
+                np.issubdtype(array.dtype, np.number)):
+            return True
+        return False
---
+++ Alfarvis/commands/Stat_ListColumns.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+"""
+Define mean command
+"""
+
+from Alfarvis.basic_definitions import (DataType, CommandStatus,
+                                        ResultObject)
+from .abstract_command import AbstractCommand
+from .argument import Argument
+from .Stat_Container import StatContainer
+import numpy as np
+
+
+class StatListColumns(AbstractCommand):
+    """
+    Compute mean of an array
+    """
+
+    def commandTags(self):
+        """
+        return tags that are used to identify mean command
+        """
+        return ["list", "columns"]
+
+    def argumentTypes(self):
+        """
+        A list of  argument structs that specify the inputs needed for
+        executing the mean command
+        """
+        return [Argument(keyword="array_data", optional=True,
+                         argument_type=DataType.csv)]
+
+    def evaluate(self, array_data):
+        """
+        List all columns in a csv matrix
+        """
+        result_object = ResultObject(None, None, None, CommandStatus.Error)
+        data = array_data.data
+        row_format = "{:>30} {:>15} {:>20}"
+        if hasattr(data, 'columns'):
+            print("Statistics for", " ".join(array_data.keyword_list))
+            print(row_format.format("Column_name", "Column_type", "Column_range"))
+            for column in data.columns:
+                data_column = data[column]
+                is_categorical = StatContainer.isCategorical(data_column)
+                if is_categorical:
+                    column_type = "Categorical"
+                elif np.issubdtype(data_column.dtype, np.number):
+                    column_type = "Number"
+                elif isinstance(data_column[0], str):
+                    column_type = "String"
+                else:
+                    column_type = "Unknown"
+                if is_categorical:
+                    unique_vals = np.unique(data_column)
+                    column_range = str(unique_vals)
+                elif np.issubdtype(data_column.dtype, np.number):
+                    column_range = "[{:.2f}, {:.2f}]".format(
+                        np.min(data_column), np.max(data_column))
+                print(row_format.format(column, column_type, column_range))
+            result_object = ResultObject(None, None, None,
+                                         CommandStatus.Success)
+
+        return result_object
