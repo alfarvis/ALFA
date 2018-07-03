57a2ec4f84c1defa30b84c6962cf8e4e04cae5f2
garimella.gowtham74@gmail.com <garimella.gowtham74@gmail.com>
2018-07-03 08:34:57 -0400

    Add tests for removing common names and is categorical

--- Alfarvis/commands/Stat_Container.py
+++ Alfarvis/commands/Stat_Container.py
@@ -16,15 +16,14 @@ class StatContainer(object):
     def removeCommonNames(self, input_names):
         name_set_list = [set(splitPattern(name)) for name in input_names]
         if len(input_names) == 1:
-            out_names = [list(name_set_list[0])[0]]
-            common_name = out_names[0]
+            out_names = ['']
+            common_name = list(name_set_list[0])
         else:
             common_name_set = set.intersection(*name_set_list)
             common_name = ' '.join(common_name_set)
             out_names = [' '.join(name_set.difference(common_name_set))
                          for name_set in name_set_list]
         return out_names, common_name
-            
 
     @classmethod
     def isCategorical(self, array):
@@ -39,7 +38,7 @@ class StatContainer(object):
             return None
         Nunique = len(uniqVals)
         N = len(array)
-        if N > 0 and isinstance(array[0], str) and Nunique < 50:
+        if (array.dtype.type is np.str_) and Nunique < 50:
             return uniqVals
         elif (Nunique / N) > self.percCutoff_for_categorical:
             return None
