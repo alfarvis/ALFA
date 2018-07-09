96710b9267cbb1a7568bd3364fccbc6026e06731
garimella.gowtham74@gmail.com <garimella.gowtham74@gmail.com>
2018-07-09 07:40:15 -0400

    Remove nans from run classifier

--- Alfarvis/commands/DataMine_RunClassifier.py
+++ Alfarvis/commands/DataMine_RunClassifier.py
@@ -14,7 +14,8 @@ from .Stat_Container import StatContainer
 import pandas as pd
 from sklearn.manifold import TSNE
 from Alfarvis.Toolboxes.DataGuru import DataGuru
-from sklearn import metrics # for the check the error and accuracy of the model
+from sklearn import metrics  # for the check the error and accuracy of the model
+
 
 class DM_Classify(AbstractCommand):
     """
@@ -25,16 +26,16 @@ class DM_Classify(AbstractCommand):
         """
         Tags to identify the train a classifier command
         """
-        return ["classify","classifier","run"]
+        return ["classify", "classifier", "run"]
 
     def argumentTypes(self):
         """
         A list of  argument structs that specify the inputs needed for
         executing the run classifier command
         """
-        #TODO Add an argument for k = number of clusters
+        # TODO Add an argument for k = number of clusters
         return [Argument(keyword="array_datas", optional=True,
-                         argument_type=DataType.array,number=-1), 
+                         argument_type=DataType.array, number=-1),
                          Argument(keyword="classifier_model", optional=True,
                          argument_type=DataType.trained_model)]
 
@@ -44,48 +45,52 @@ class DM_Classify(AbstractCommand):
 
         """
         result_object = ResultObject(None, None, None, CommandStatus.Error)
-        
+
         # Get the data frame
         sns.set(color_codes=True)
         command_status, df, kl1 = DataGuru.transformArray_to_dataFrame(array_datas)
         if command_status == CommandStatus.Error:
             return ResultObject(None, None, None, CommandStatus.Error)
-        
-        if StatContainer.ground_truth is not None:
-            df = DataGuru.removeGT(df,StatContainer.ground_truth)
-        
-        # Get the classifier model
-        trained_model = classifier_model.data
-        model = trained_model['Model']
-        scaler = trained_model['Scaler']
-        #Scale the values based on the training standardizer
-        X = df.values
-        X = scaler.transform(X)
-        
-        #Code to run the classifier
-        
+
         if StatContainer.ground_truth is None:
             print("Please set a feature vector to ground truth by typing set ground truth to",
                   "to get the prediction accuracy")
             result_object = ResultObject(None, None, None, CommandStatus.Error)
             return result_object
         else:
+            df = DataGuru.removeGT(df, StatContainer.ground_truth)
             Y = StatContainer.ground_truth.data
-        
-        
-        
-        #Plot the k -means result
+
+        # Remove NAN
+        df['ground_truth'] = Y
+        df_remove_nan = df.dropna()
+
+        # Get inputs
+        df_drop = df_remove_nan.drop('ground_truth', axis=1)
+        X = df_drop.values
+        Y = df_remove_nan['ground_truth']
+
+        # Get the classifier model
+        trained_model = classifier_model.data
+        model = trained_model['Model']
+        scaler = trained_model['Scaler']
+
+        # Scale the values based on the training standardizer
+        X = scaler.transform(X)
+
+        # Code to run the classifier
+        # Plot the k -means result
         print('Running the trained classifier...')
-        
+
         predictions = model.predict(X)
         accuracy = metrics.accuracy_score(predictions, Y)
         print("Accuracy : %s" % "{0:.3%}".format(accuracy))
-        cm = metrics.confusion_matrix(Y,predictions)
-        DataGuru.plot_confusion_matrix(cm,np.unique(Y),title="confusion matrix")
+        cm = metrics.confusion_matrix(Y, predictions)
+        DataGuru.plot_confusion_matrix(cm, np.unique(Y), title="confusion matrix")
         plt.show(block=False)
-        
-        #TODO Need to save the model
-        #Ask user for a name for the model
-        result_object = ResultObject(None, None, None,CommandStatus.Success)    
+
+        # TODO Need to save the model
+        # Ask user for a name for the model
+        result_object = ResultObject(None, None, None, CommandStatus.Success)
 
         return result_object
