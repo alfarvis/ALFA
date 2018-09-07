47af0c35d764a2893891bfe51e98690e0f63b0b3
garimella.gowtham74@gmail.com <garimella.gowtham74@gmail.com>
2018-09-06 21:41:52 -0400

    Add description

--- Alfarvis/commands/PrintSummary.py
+++ Alfarvis/commands/PrintSummary.py
@@ -14,6 +14,7 @@ from .Stat_Container import StatContainer
 from Alfarvis.Toolboxes.DataGuru import DataGuru
 import scipy
 
+
 class DataSummary(AbstractCommand):
     """
     Print summary
@@ -49,9 +50,7 @@ class DataSummary(AbstractCommand):
         df_new['stdev'] = df.std().values
         df_new['min'] = df.min().values
         df_new['max'] = df.max().values
-        
-        
-        
+
         return df_new
 
     def evaluate(self, data_frame):
@@ -61,17 +60,20 @@ class DataSummary(AbstractCommand):
 
         """
         result_object = ResultObject(None, None, None, CommandStatus.Success)
-        
-        df_new = self.performOperation(data_frame.data)            
+
+        df_new = self.performOperation(data_frame.data)
         TablePrinter.printDataFrame(df_new)
-        
+
         return result_object
-            
+
 
 class DataGroupSummary(DataSummary):
 
     def __init__(self):
-        super(DataGroupSummary, self).__init__(["groupwise","labelwise","summary"])
+        super(DataGroupSummary, self).__init__(["groupwise", "labelwise", "summary", "label wise", "group wise"])
+
+    def briefDescription(self):
+        return "print label wise summary"
 
     def performOperation(self, df):
         if StatContainer.ground_truth is None or len(StatContainer.ground_truth.data) != df.shape[0]:
@@ -82,7 +84,7 @@ class DataGroupSummary(DataSummary):
             gtName = StatContainer.ground_truth.name
         df[gtName] = gtVals
         uniqVals = StatContainer.isCategorical(gtVals)
-        df_new = pd.DataFrame()                
+        df_new = pd.DataFrame()
         if uniqVals is not None:
             if gtName in df.columns:
                 df_new['features'] = df.columns.drop(gtName).values
@@ -92,22 +94,22 @@ class DataGroupSummary(DataSummary):
             gb_mean = gb.mean().transpose()
             gb_std = gb.std().transpose()
             for iter in range(len(uniqVals)):
-                df_new[str(uniqVals[iter])+'_mean'] = gb_mean.values[:,iter]
-                df_new[str(uniqVals[iter])+'_stdev'] = gb_std.values[:,iter]
+                df_new[str(uniqVals[iter]) + '_mean'] = gb_mean.values[:, iter]
+                df_new[str(uniqVals[iter]) + '_stdev'] = gb_std.values[:, iter]
             for iter in range(len(uniqVals)):
                 for iter1 in range(len(uniqVals)):
-                    df_new['pValue: ' +str(iter)+' vs ' + str(iter1)] = np.zeros(df_new.shape[0])   
+                    df_new['pValue: ' + str(iter) + ' vs ' + str(iter1)] = np.zeros(df_new.shape[0])
             allCols = df_new['features']
             for iter_feature in range(len(df_new['features'])):
-                arr = df[allCols[iter_feature]]                
-                for iter in range(len(uniqVals)):                
+                arr = df[allCols[iter_feature]]
+                for iter in range(len(uniqVals)):
                     uniV = uniqVals[iter]
-                    a = arr[gtVals==uniV]
+                    a = arr[gtVals == uniV]
                     for iter1 in range(len(uniqVals)):
-                        b = arr[gtVals==uniqVals[iter1]]
-                        if uniV != uniqVals[iter1]:                        
+                        b = arr[gtVals == uniqVals[iter1]]
+                        if uniV != uniqVals[iter1]:
                             ttest_val = scipy.stats.ttest_ind(a, b, axis=0, equal_var=False)
-                            df_new['pValue: ' +str(iter)+' vs ' +str(iter1)][iter_feature]=(ttest_val.pvalue)
+                            df_new['pValue: ' + str(iter) + ' vs ' + str(iter1)][iter_feature] = (ttest_val.pvalue)
                         else:
-                            df_new['pValue: ' +str(iter)+' vs ' +str(iter1)][iter_feature]=0 
+                            df_new['pValue: ' + str(iter) + ' vs ' + str(iter1)][iter_feature] = 0
         return df_new
