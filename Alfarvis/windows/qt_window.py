2b60c27f0bcb9c512e204faf7a78e35ad598d2aa
garimella.gowtham74@gmail.com <garimella.gowtham74@gmail.com>
2018-09-15 23:32:31 -0400

    Fix max tab count and rotate tabs as we go on

--- Alfarvis/windows/qt_window.py
+++ Alfarvis/windows/qt_window.py
@@ -6,28 +6,45 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 
 
+def clearLayout(layout):
+    while layout.count():
+        child = layout.takeAt(0)
+        layout.removeItem(child)
+
+
 class QtWindow(AbstractWindow):
     """
     Simple window that is the same as matplotlib figure window
     """
     parent_tab_widget = None
+    max_tab_count = 7
+    current_index_count = 0
 
     def __init__(self, *args, **kwargs):
         if self.parent_tab_widget is None:
             QtWindow.parent_tab_widget = QTabWidget()
-        QtWindow.parent_tab_widget.setTabsClosable(True)
-        QtWindow.parent_tab_widget.setMovable(True)
-        self.figure_tab = QWidget()
+            QtWindow.parent_tab_widget.setMovable(True)
+        # Create tab widget
+        self.tab_index = QtWindow.current_index_count
+        if self.parent_tab_widget.count() < self.max_tab_count:
+            self.figure_tab = QWidget()
+            self.parent_tab_widget.addTab(self.figure_tab,
+                                          "Figure " + str(self.tab_index + 1))
+            self.layout = QVBoxLayout()
+        else:
+            self.figure_tab = self.parent_tab_widget.widget(QtWindow.current_index_count)
+            self.layout = self.figure_tab.layout()
+            clearLayout(self.layout)
+
+        # Create Figure
         self.figure = plt.figure(*args, **kwargs)
         self.canvas = FigureCanvas(self.figure)
         self.toolbar = NavigationToolbar(self.canvas, self.figure_tab)
-        layout = QVBoxLayout()
-        layout.addWidget(self.toolbar)
-        layout.addWidget(self.canvas)
-        self.figure_tab.setLayout(layout)
-        self.tab_count = self.parent_tab_widget.count()
-        self.parent_tab_widget.addTab(self.figure_tab,
-                                      "Figure " + str(self.tab_count + 1))
+        self.layout.addWidget(self.toolbar)
+        self.layout.addWidget(self.canvas)
+        self.figure_tab.setLayout(self.layout)
+        # Update index and connect event
+        QtWindow.current_index_count = (QtWindow.current_index_count + 1) % QtWindow.max_tab_count
         self.canvas.mpl_connect('resize_event', self.tight_layout)
 
     def tight_layout(self, event):
@@ -41,9 +58,10 @@ class QtWindow(AbstractWindow):
         self.parent_tab_widget = parent_widget
 
     def show(self):
+        # self.figure_tab.setLayout(self.layout)
         self.figure.tight_layout()
         self.canvas.draw()
-        self.parent_tab_widget.setCurrentIndex(self.tab_count)
+        self.parent_tab_widget.setCurrentIndex(self.tab_index)
 
     def gcf(self):
         return self.figure
