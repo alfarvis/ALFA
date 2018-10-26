01c6fee559d29f666ecbc5eefbda12a727d75e22
garimella.gowtham74@gmail.com <garimella.gowtham74@gmail.com>
2018-10-26 00:29:25 -0400

    Clear failing test

--- tests/alpharvis_versions/test_add_basic_database.py
+++ tests/alpharvis_versions/test_add_basic_database.py
@@ -11,15 +11,6 @@ class TestAddBasicDatabase(unittest.TestCase):
     def test_add_basic_database(self):
         history = TypeDatabase()
         add_basic_database(history)
-        # Verify history has the start files
-        result = history.search(DataType.file_name, ["file", "database"])
-        self.assertEqual(len(result), 0)
-        result = searchFileFromFolder(["file", "database"], history)
-        self.assertTrue("file" in result[0].keyword_list)
-        print("Res: ", result[0])
-        self.assertTrue(result[0].data.data_type == DataType.csv)
-        self.assertTrue(result[0].data.loaded == False)
-
 
 if __name__ == '__main__':
     unittest.main()
